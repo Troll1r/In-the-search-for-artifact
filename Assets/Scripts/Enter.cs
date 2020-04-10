408c3b3ba8d90d0c0734ecbec6e6520a9764040e
Troll1r <kiralpa1503@gmail.com>
2020-04-10 20:24:31 +0400

    1
    Description

---
+++ Assets/Scripts/Enter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enter : MonoBehaviour
+{
+    public GameObject Stone;
+    public GameObject Player;
+    void Start()
+    {
+
+        
+    }
+
+
+    void OnTriggerEnter2D(Collider2D myTrigger)
+    {
+        if (myTrigger.gameObject == Player)
+        {
+            Stone.SetActive(true);
+            print("f");
+
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    public float Move;
+    public float jump;
+    float speed;
+    public bool Grounded = false;
+    public Transform GroundCheck;
+    public float GroundRadius = 0.2f;
+    public LayerMask WhatGround;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+
+    void Update()
+    {
+        Grounded = Physics2D.OverlapCircle(GroundCheck.position, GroundRadius, WhatGround);
+        if (Input.GetKey(KeyCode.D))
+        {
+            speed = Move;
+
+            Vector3 Scaler = transform.localScale;
+            Scaler.x = 1;
+            transform.localScale = Scaler;
+
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            speed = -Move;
+            Vector3 Scaler = transform.localScale;
+            Scaler.x = -1;
+            transform.localScale = Scaler;
+
+        }
+
+        
+        if (Input.GetKeyDown(KeyCode.Space) && Grounded)
+        {
+            rb.AddForce(new Vector2(0, jump),ForceMode2D.Impulse);
+
+        }
+
+        transform.Translate(speed, 0, 0);
+        speed = 0;
+    }
+
+
+
+}
